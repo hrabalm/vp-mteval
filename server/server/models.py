7b920cf78032c7525314dc6049f35207fbd0443d
Miroslav Hrabal <mhn@email.cz>
2025-04-27 23:56:00 +0200

    update models

--- server/server/models.py
+++ server/server/models.py
@@ -1,18 +1,28 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import datetime
+
+from sqlalchemy import JSON, DateTime, ForeignKey, Text, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
-class Base(DeclarativeBase): ...
+class Base(DeclarativeBase):
+    __abstract__ = True
+    creation_date: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now(), nullable=False
+    )
+    last_update_date: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now(), onupdate=func.now(), nullable=False
+    )
 
 
 class TodoItem(Base):
     __tablename__ = "todo_items"
 
     title: Mapped[str] = mapped_column(primary_key=True)
-    done: Mapped[bool]
+    done: Mapped[bool] = mapped_column(nullable=False)
 
 
 # TODO:
-# - How do I store mnetrics data?:
+# - How do I store metrics data?:
 #   - Sentence level / Document level / special (n-grams)
 #   - I need a mechanism to store arbitrary JSON data for all segments, documents or datasets
 #   - Add creation date and last update date to Base?
@@ -22,23 +32,37 @@ class TodoItem(Base):
 
 class Segment(Base):
     __tablename__ = "segments"
+    __table_args__ = ()
 
-    src: Mapped[str]
-    tgt: Mapped[str]
-
-
-class Document(Base):
-    __tablename__ = "documents"
-
-    segments: list[Segment]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    dataset_id: Mapped[int] = mapped_column(
+        ForeignKey("datasets.id"), ondelete="CASCADE", nullable=False
+    )
+    src: Mapped[str] = mapped_column(Text, nullable=False)
+    tgt: Mapped[str] = mapped_column(Text, nullable=False)
 
 
 class Dataset(Base):
-    __tablename__ = "datasets"
+    """A dataset is a collection of segments. If document level information
+    becomes represent, we will represent it as an optional mapping in another
+    table."""
 
-    documents: list[Document]
-    languages: Mapped[str]
-    # namespace:
+    __tablename__ = "datasets"
+    id = mapped_column(primary_key=True)
+    namespace_id: Mapped[int] = mapped_column(
+        ForeignKey("namespaces.id"), ondelete="CASCADE", nullable=False
+    )
+    name: Mapped[str] = mapped_column(Text, nullable=False)
+    data_hash: Mapped[str] = mapped_column(Text, nullable=False)
+
+    source_lang: Mapped[str] = mapped_column(Text, nullable=False)  # iso639-1 code
+    target_lang: Mapped[str] = mapped_column(Text, nullable=False)  # iso639-1 code
+
+    segments: Mapped[list[Segment]] = relationship(
+        "Segment",
+        back_populates="dataset",
+        cascade="all, delete-orphan",
+    )
 
 
 # Translations
@@ -59,12 +83,17 @@ class TranslationRun(Base):
 
 class SegmentMetric(Base):
     __tablename__ = "segment_metrics"
-    pass
 
-
-class DocumentMetric(Base):
-    __tablename__ = "document_metrics"
-    pass
+    score: Mapped[float] = mapped_column(nullable=False)
+    higher_is_better: Mapped[bool] = mapped_column(nullable=False)
+    segment_id: Mapped[int] = mapped_column(
+        ForeignKey("segments.id"), ondelete="CASCADE", nullable=False
+    )
+    segment: Mapped[Segment] = relationship(
+        "Segment",
+        back_populates="metrics",
+        cascade="all, delete-orphan",
+    )
 
 
 class DatasetMetric(Base):
@@ -85,19 +114,22 @@ class Namespace(Base):
 
     __tablename__ = "namespaces"
 
-    name: Mapped[str] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(Text, nullable=False)
+
+    # TODO: add many to many relationship to users with R or R/W access
 
 
 class User(Base):
     __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(primary_key=True)
-    # email:
-    # password:
+    email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
+    password_hash: Mapped[str] = mapped_column(Text, nullable=False)
 
 
 # TODO: jobs
-# - we need to support queues (CPU and GPU)
+# - we need to support queues (at least CPU and GPU)
 # - we might want to support priorities - low/default/high
 # - user and namespace (so that the job runner can choose to filter jobs by it)
 
