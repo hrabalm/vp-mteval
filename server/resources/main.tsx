1dc9752482865ebee5ac836ba2e1978fabcbedbf
Miroslav Hrabal <mhn@email.cz>
2025-05-19 15:55:35 +0200

    fix: devcontainer configuration

--- server/resources/main.tsx
+++ server/resources/main.tsx
@@ -1,7 +1,7 @@
-import "./styles.css"
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import "./styles.css"
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
