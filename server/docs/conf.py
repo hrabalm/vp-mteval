e728170e76903ebfc0b659a28876c002e0f6d50b
Miroslav Hrabal <mhn@email.cz>
2025-05-24 17:45:40 +0200

    feat: update docs configuration

--- server/docs/conf.py
+++ server/docs/conf.py
@@ -6,22 +6,45 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'VP-MTEval'
-copyright = '2025, Miroslav Hrabal'
-author = 'Miroslav Hrabal'
+project = "VP-MTEval"
+copyright = "2025, Miroslav Hrabal"
+author = "Miroslav Hrabal"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_click",
+    "sphinx_design",
+    "sphinx.ext.todo",
+    "sphinx_copybutton",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib.mermaid",
+    "sphinx.ext.intersphinx",
+    "sphinx_toolbox.collapse",
+    "sphinx.ext.autosectionlabel",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+# -- Style configuration -----------------------------------------------------
+html_theme = "shibuya"
+html_static_path = ["_static"]
+html_show_sourcelink = True
+html_title = "VP-MTEval Server Docs"
+html_context = {
+    "github_user": "hrabalm",
+    "github_repo": "vp-mteval",
+    "github_version": "main",
+    "doc_path": "server/docs",
+}
