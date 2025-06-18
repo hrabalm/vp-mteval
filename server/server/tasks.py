b8ea53be1a23c96d2d70718b10c22a60f8f25a8b
Miroslav Hrabal <mhn@email.cz>
2025-06-19 00:53:13 +0200

    fix: formatting

--- server/server/tasks.py
+++ server/server/tasks.py
@@ -7,15 +7,18 @@ import logging
 
 logger = logging.getLogger("server.tasks")
 
+
 async def _cleanup_expired_workers_and_jobs(transaction):
     now = datetime.datetime.now(datetime.timezone.utc)
-    expiration_datetime = now - datetime.timedelta(seconds=settings.worker_expiration_seconds)
+    expiration_datetime = now - datetime.timedelta(
+        seconds=settings.worker_expiration_seconds
+    )
     expiration_timestamp = expiration_datetime.timestamp()
 
     # select expired workers
     query = select(models.Worker).filter(
-            models.Worker.last_heartbeat < expiration_timestamp,
-            models.Worker.status == models.WorkerStatus.WORKING
+        models.Worker.last_heartbeat < expiration_timestamp,
+        models.Worker.status == models.WorkerStatus.WORKING,
     )
     result = await transaction.execute(query)
     expired_workers = result.scalars().all()
@@ -25,9 +28,9 @@ async def _cleanup_expired_workers_and_jobs(transaction):
             worker.status = models.WorkerStatus.TIMED_OUT
         await transaction.flush()
 
+
 async def cleanup_expired_workers_and_jobs_task(_):
     logger.info("Starting cleanup of expired workers and jobs...")
     async with plugins.db_config.get_session() as session:
         async with session.begin():
             await _cleanup_expired_workers_and_jobs(session)
-
