fdf4a803977c1f0f17e3e4d573cdfb28f84e7473
Miroslav Hrabal <mhn@email.cz>
2025-06-23 07:37:56 +0200

    fix: ngrams fetching code

--- server/resources/runs.tsx
+++ server/resources/runs.tsx
@@ -15,6 +15,18 @@ export const fetchRun = async (runId: string, namespaceId: string) => {
     return run;
 }
 
+export const fetchRunNGrams = async (runId: string, namespaceId: string) => {
+    const run = await api
+        .get(`api/v1/namespaces/${namespaceId}/translations-runs/${runId}/ngrams/`)
+        .then((r) => r.data).catch((err) => {
+            if (err.status === 404) {
+                throw new RunNotFoundError(`Run with id "${runId}" not found in namespace "${namespaceId}"`);
+            }
+            throw err;
+        })
+    return run;
+}
+
 export const fetchRuns = async (namespaceId: string) => {
     return api
         .get(`api/v1/namespaces/${namespaceId}/translations-runs/`)
